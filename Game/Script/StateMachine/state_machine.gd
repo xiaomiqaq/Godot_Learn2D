32c188f78d4a040b6809dce50b18c4c42e4f6496
zpl <xiaomiqaq@163.com>
2026-03-21 11:50:07 +0800

    add state machine

--- Game/Script/StateMachine/state_machine.gd
+++ Game/Script/StateMachine/state_machine.gd
@@ -1,2 +1,15 @@
 extends Node
 class_name StateMachine
+
+
+var currentState: State
+
+func _ready() -> void:
+	for child in get_children():
+		var childState = child as State
+		childState.parentStateMachine = self
+		childState.Ready()
+	currentState = get_child(0)
+	currentState.Enter()
+	
+		
